0208173820586af6908eef283f9fde6558174975
Juan Sebastian Cardona Morales <juan.cardona@yuxiglobal.com>
2018-11-02 17:17:31 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CompiladoresFinalProject';
+  activeFilter = true;
+
+  allPlaces(): void {
+    this.activeFilter = !this.activeFilter;
+  }
+
+  filterByMyOwnPlaces(): void {
+    this.activeFilter = !this.activeFilter;
+  }
 }
